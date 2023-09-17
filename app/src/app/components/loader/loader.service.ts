85aa2ea0363d3338135210fe80feb739305190b0
Cornel <corneltolmay@gmail.com>
2023-09-17 23:21:30 +0200

    removed API key

--- app/src/app/components/loader/loader.service.ts
+++ app/src/app/components/loader/loader.service.ts
@@ -8,6 +8,9 @@ import { LoaderComponent } from './loader.component';
 })
 export class LoaderService { // This service is to control when the loader displays or doesn't
 
+  // It's better to use a global service, where the service can check whether the loader is
+  // displaying or not, to prevent loader instances from being stacked.
+
   loaderVisible : boolean = false;
 
   ref: DynamicDialogRef | undefined;
